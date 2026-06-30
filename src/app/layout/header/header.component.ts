6c213198e9c8b4ea8391d0548d6235c327cf1c89
GuadalupeVeliz <guadalupeveliz100@gmail.com>
2026-06-30 00:31:27 -0300

    se agregan items y dropdowns al navbar

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { LucideLogOut, LucideMap, LucideUser} from '@lucide/angular';
+import { LucideLogOut, LucideMap, LucideUser, LucidePackageOpen, LucidePlane, LucideReceiptText, LucideUsers, LucideChartColumn } from '@lucide/angular';
 
 @Component({
   selector: 'app-header',
-  imports: [LucideLogOut, LucideMap,LucideUser],
+  imports: [LucideLogOut, LucideMap, LucideUser, LucidePackageOpen, LucidePlane, LucideReceiptText, LucideUsers, LucideChartColumn],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
