af135bca843edfb9c205905b256dd1f8e6b810d7
Nicolas-Barboza <nicobarboza85@gmail.com>
2026-07-07 02:41:23 -0300

    feat: agregar componente Estadisticaa con su plantilla y estilos

---
+++ src/app/pages/admin/estadisticaa/estadisticaa.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-estadisticaa',
+  imports: [],
+  templateUrl: './estadisticaa.component.html',
+  styleUrl: './estadisticaa.component.css',
+})
+export class EstadisticaaComponent {}
