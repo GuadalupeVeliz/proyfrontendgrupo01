70d18854743e098ec991f8b156e0951fac9bfd6d
JuanLamas <juanedulam2@gmail.com>
2026-07-07 12:01:22 -0300

    Se agrega funciones para registrar reservas

--- src/app/pages/vacantes/vacantes.component.ts
+++ src/app/pages/vacantes/vacantes.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
 import { VacanteService } from '../../core/services/vacante.service';
+import { Vacante } from '../../models/vacante.interface';
+import { ReservaRequest } from '../../models/reserva.interface';
+import { FormsModule } from '@angular/forms';
+import { ReservaService } from '../../core/services/reserva.service';
 
 @Component({
   selector: 'app-vacantes',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './vacantes.component.html',
   styleUrl: './vacantes.component.css',
 })
@@ -13,12 +17,21 @@ export class VacantesComponent implements OnInit {
   paqueteId!: number;
   paquete: any = null;
   vacantes: any[] = [];
+  vacanteSeleccionada: any;
 
+  reservaModel: ReservaRequest = {
+    fechaDeReservacion: '',
+    cantidadDePersonas: 1,
+    clienteId: Number(localStorage.getItem('idCliente')),
+    vacanteId: 0
+  };
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private vacanteService: VacanteService,
-    public authService: AuthService
-  ) {}
+    public authService: AuthService,
+    private reservaService: ReservaService,
+  ) { }
 
   ngOnInit(): void {
     this.paqueteId = Number(this.route.snapshot.paramMap.get('id'));
@@ -38,5 +51,40 @@ export class VacantesComponent implements OnInit {
         console.error('Error al obtener vacantes', error);
       },
     });
+  }
+
+  seleccionarVacante(vacante : Vacante) {
+    this.vacanteSeleccionada=vacante;
+    this.reservaModel.fechaDeReservacion= vacante.fechaDeSalida;
+    this.reservaModel.vacanteId = Number(vacante.id)
+    console.log(vacante)
+  }
+
+  registrarReserva(): void {
+    console.log(this.reservaModel)
+    this.reservaService.createReserva(this.reservaModel).subscribe({
+      next: () => {
+        (document.getElementById('btnCancelarModal') as HTMLButtonElement).click();
+        this.router.navigate([`/`]);
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
+
+  aumentarCantidad(): void {
+    if (
+      this.vacanteSeleccionada &&
+      this.reservaModel.cantidadDePersonas < this.vacanteSeleccionada.cupoDisponible
+    ) {
+      this.reservaModel.cantidadDePersonas++;
+    }
+  }
+
+  disminuirCantidad(): void {
+    if (this.reservaModel.cantidadDePersonas > 1) {
+      this.reservaModel.cantidadDePersonas--;
+    }
   }
 }
