c3c59ba9d0c69b7b721879a37740b412eded460b
GuadalupeVeliz <guadalupeveliz100@gmail.com>
2026-07-07 20:03:42 -0300

    arreglo url backend

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://proybackendgrupo01-ecl4.onrender.com' || 'http://localhost:3000/api/v1',
+  apiUrl: 'https://proybackendgrupo01-ecl4.onrender.com/api/v1',
   clientId: '352988450917-kltn34b3fgpc7ko2lrveq5ee7k48rs8r.apps.googleusercontent.com',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://proybackendgrupo01-ecl4.onrender.com' || 'http://localhost:3000/api/v1',
+  apiUrl: 'https://proybackendgrupo01-ecl4.onrender.com/api/v1',
 };
