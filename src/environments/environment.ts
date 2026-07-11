4ec9440c5078e01151ba2f5d3514c03c6c29dd91
Nicolas-Barboza <nicobarboza85@gmail.com>
2026-07-10 21:20:34 -0300

    fix(environment): update apiUrl configuration for development and production environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/api/v1',
-  // apiUrl: 'https://proybackendgrupo01-ecl4.onrender.com/api/v1',
+  //apiUrl: 'http://localhost:3000/api/v1',
+  apiUrl: 'https://proybackendgrupo01-ecl4.onrender.com/api/v1',
   clientId: '352988450917-kltn34b3fgpc7ko2lrveq5ee7k48rs8r.apps.googleusercontent.com',
 };
