2171ebbf6f869d600d0b54675948ba257a029f23
GuadalupeVeliz <guadalupeveliz100@gmail.com>
2026-07-11 05:41:19 -0300

    agrego urls

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   // apiUrl: 'http://localhost:3000/api/v1',
-  // apiUrl: 'https://proybackendgrupo01-ecl4.onrender.com/api/v1',
-  apiUrl: 'https://marilyn-specials-west-artist.trycloudflare.com/api/v1',
+  apiUrl: 'https://proybackendgrupo01-ecl4.onrender.com/api/v1',
+  // apiUrl: 'https://marilyn-specials-west-artist.trycloudflare.com/api/v1',
   clientId: '352988450917-kltn34b3fgpc7ko2lrveq5ee7k48rs8r.apps.googleusercontent.com',
 };
